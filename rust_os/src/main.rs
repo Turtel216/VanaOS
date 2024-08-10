f4b2f4c7d076d2b933d29bcf4ea749e6d42636ef
Turtel216 <papadimitrios216@gmail.com>
2024-08-10 08:46:18 +0300

    Booted to qenu

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -9,9 +9,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Entry Point
 #[no_mangle] // don't auto generate function names
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 //############
