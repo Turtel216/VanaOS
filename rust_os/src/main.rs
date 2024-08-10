2ed7e66ded5e6f7861b6c0bd281ade623f9afd7c
Turtel216 <papadimitrios216@gmail.com>
2024-08-10 09:22:56 +0300

    Added vga support

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -3,25 +3,24 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 // Entry Point
 #[no_mangle] // don't auto generate function names
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    use core::fmt::Write;
+    vga_buffer::WRITER
+        .lock()
+        .write_str("Hello Kernel!")
+        .unwrap();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 15.3).unwrap();
 
     loop {}
 }
