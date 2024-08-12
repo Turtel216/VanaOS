9a3668887ed7e00f5337ba196cb7a076ed357637
Turtel216 <papadimitrios216@gmail.com>
2024-08-12 07:27:17 +0300

    Added vga buffer tests

--- rust_os/src/vga_buffer.rs
+++ rust_os/src/vga_buffer.rs
@@ -141,3 +141,25 @@ impl fmt::Write for Writer {
         Ok(())
     }
 }
+
+#[test_case]
+fn test_println_simple() {
+    kprintln!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        kprintln!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    kprintln!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
