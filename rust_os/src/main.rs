101291609a739031fd2f56e22634b2231434448a
Turtel216 <papadimitrios216@gmail.com>
2024-10-05 01:24:52 +0300

    Added bump allocator

--- rust_os/src/main.rs
+++ rust_os/src/main.rs
@@ -6,7 +6,6 @@
 
 extern crate alloc;
 
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::{allocator, kprintln, memory::BootInfoFrameAllocator};
@@ -25,30 +24,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("Heap initialization failed");
-
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    kprintln!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    kprintln!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    kprintln!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    kprintln!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
 
     #[cfg(test)]
     test_main();
